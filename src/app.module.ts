2f3f3fcff86feee173b1ed13c2c7892282af5034
dojoohwan <ehwnghks@gmail.com>
2023-11-10 15:28:38 +0900

    users module, service 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { ChannelController } from './channel/channel.controller';
 import { ChannelService } from './channel/channel.service';
 import { ChannelModule } from './channel/channel.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [SearchModule, ChannelModule, AuthModule],
+  imports: [SearchModule, ChannelModule, AuthModule, UsersModule],
   controllers: [AppController, SearchController, ChannelController],
   providers: [AppService, SearchService, ChannelService],
 })
